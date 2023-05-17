a71f3c08e8e4f7f18349142de0f8b8160a6556bd
alel2104 <alel2104@student.miun.se>
2023-05-17 16:51:45 +0200

    basdasd

--- src/main/java/com/dt183g/project/Main.java
+++ src/main/java/com/dt183g/project/Main.java
@@ -13,19 +13,19 @@ public class Main {
         BufferedImage mazeImage = fileReader.readMazeImage("med7.jpg");
         MazeWindow window = new MazeWindow(mazeImage);
 
-        int[][] test = fileReader.getMatrixFromImage(mazeImage);
-        //System.out.println(test.length);
-
-        for (int i = 0; i < test.length; i++) {
-            for (int j = 0; j < test[i].length; j++) {
-//                if (test[i][j] == 1) {
-//                    System.out.printf("%d", test[i][j]);
-//                } else {
-//                    System.out.print(" ");
-//                }
-                System.out.printf("%d", test[i][j]);
-            }
-            System.out.println();
-        }
+//        int[][] test = fileReader.getMatrixFromImage(mazeImage);
+//        //System.out.println(test.length);
+//
+//        for (int i = 0; i < test.length; i++) {
+//            for (int j = 0; j < test[i].length; j++) {
+////                if (test[i][j] == 1) {
+////                    System.out.printf("%d", test[i][j]);
+////                } else {
+////                    System.out.print(" ");
+////                }
+//                System.out.printf("%d", test[i][j]);
+//            }
+//            System.out.println();
+//        }
     }
 }
--- src/main/java/com/dt183g/project/MazeWindow.java
+++ src/main/java/com/dt183g/project/MazeWindow.java
@@ -5,19 +5,24 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 public class MazeWindow {
+    private BufferedImage mazeImage;
     private JFrame window;
     private JLabel imageLabel;
     private JPanel imagePanel;
 
     public MazeWindow(BufferedImage maze) {
-        initializeWindow(maze);
+        this.mazeImage = maze;
+        initializeWindow();
+        addMazePosition(50, 50);
     }
 
-    private void initializeWindow(BufferedImage maze) {
-        initializeImagePanel(maze);
+    private void initializeWindow() {
+        initializeImagePanel();
         window = new JFrame();
         window.add(imagePanel);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,9 +32,18 @@ public class MazeWindow {
         window.setLocationRelativeTo(null);
     }
 
-    private void initializeImagePanel(BufferedImage maze) {
-        imageLabel = new JLabel("", new ImageIcon(maze), SwingConstants.CENTER);
+    private void initializeImagePanel() {
+        imageLabel = new JLabel("", new ImageIcon(mazeImage), SwingConstants.CENTER);
         imagePanel = new JPanel();
         imagePanel.add(imageLabel);
+    }
+
+    private void addMazePosition(int x, int y) {
+        Graphics g2d = mazeImage.getGraphics();
+        g2d.setColor(Color.red);
+        g2d.fillRect(x, y, 5, 5);
+        g2d.dispose();
+
+        imageLabel.repaint();
     }
 }
